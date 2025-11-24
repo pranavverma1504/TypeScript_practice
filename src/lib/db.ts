d6ccfa23e6defa0a8a2c4949bd541aba5dc8c2fe
pranav verma <vermapranav001@gmail.com>
2025-11-25 01:47:32 +0530

    created registeration API route in api folder

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,30 +1,30 @@
 import { connect } from "mongoose";
 
-let mongodbUrl = process.env.MONGODB_URL 
+let mongodbUrl = process.env.MONGODB_URL;
 if (!mongodbUrl) {
-    throw new Error("MONGODB_URL is not defined");
+  throw new Error("MONGODB_URL is not defined");
 }
 
-let cached = global.mongoose
+let cached = global.mongoose;
 
 if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
-const connectDb= async () => {
-    if (cached.conn) {
-        return cached.conn;
-    }
-
-    if (!cached.promise) {
-        cached.promise= connect(mongodbUrl).then((c)=>c.connection);
-    }
-   
-    try {
-        cached.conn = await cached.promise;
-    } catch (error){
-        throw error;
-    }
+const connectDb = async () => {
+  if (cached.conn) {
     return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = connect(mongodbUrl).then((c) => c.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    throw error;
+  }
+  return cached.conn;
 };
 export default connectDb;
