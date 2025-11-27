8be466d341da185551597b85188dee6f7c689e2d
pranav verma <vermapranav001@gmail.com>
2025-11-27 18:58:54 +0530

    made homepage and showed session's data using useSession()

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+images:{
+  remotePatterns:[
+    {
+hostname:"lh3.googleusercontent.com"
+    },
+    {hostname:"res.cloudinary.com"}
+    
+  ]
+}
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+'use client'
+import { signOut, useSession } from 'next-auth/react'
+import React, {  useState } from 'react'
+import Image from 'next/image'
+import { HiPencil } from "react-icons/hi2";
+import { useRouter } from 'next/navigation';
+function Page() {
+  
+const {data}=useSession()
+  const router =useRouter()
+  const [loading,setLoading]=useState(false)
 
-const page = () => {
+  const handleSignOut=async ()=>{
+    setLoading(true)
+    try {
+      await signOut()
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+    }
+  }
   return (
-    <div>page</div>
+    <div className='min-h-screen flex flex-col items-center justify-center bg-black text-white px-4 '>
+      {data &&
+        <div className='w-full max-w-md border-2 border-white rounded-2xl p-8 shadow-lg text-center relative flex flex-col items-center'>
+          <HiPencil size={22} color='white' className='absolute right-[20px] top-[20px] cursor-pointer' onClick={()=>router.push("/edit")}/>
+           {data.user?.image && <div className='relative w-[200px] h-[200px] rounded-full border-2 border-white overflow-hidden'>
+            <Image src={data.user.image} fill alt='userImage'/>
+            </div>}
+            <h1 className='text-2xl font-semibold my-4'>Welcome, {data.user?.name}</h1>
+            <button className='w-full py-2 px-4 bg-white text-black font-semibold rounded-lg hover:bg-gray-200 transition-colors' onClick={handleSignOut}>Sign Out</button>
+        </div>}
+      {!data && <div className='text-white text-2xl'>Loading...</div>}
+
+    </div>
   )
 }
 
-export default page
+export default Page
